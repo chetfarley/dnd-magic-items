1e2b228220b662841527a896858c1e3751b69e92
Chet Farley <chetfarley@gmail.com>
2016-04-12 15:27:40 -0500

    Update core files to fix build errors
    Removed the bootstrap/docs folder which was causing jekyll to throw a
    page build failure.

---
+++ _site/assets/js/application.js
@@ -0,0 +1,12 @@
+// Individual Bootstrap Scripts
+// @codekit-prepend "../../lib/bootstrap/js/affix.js";
+// @codekit-prepend "../../lib/bootstrap/js/alert.js";
+// @codekit-prepend "../../lib/bootstrap/js/button.js";
+// @codekit-prepend "../../lib/bootstrap/js/carousel.js";
+// @codekit-prepend "../../lib/bootstrap/js/dropdown.js";
+// @codekit-prepend "../../lib/bootstrap/js/modal.js";
+// @codekit-prepend "../../lib/bootstrap/js/popover.js";
+// @codekit-prepend "../../lib/bootstrap/js/scrollspy.js";
+// @codekit-prepend "../../lib/bootstrap/js/tab.js";
+// @codekit-prepend "../../lib/bootstrap/js/tooltip.js";
+// @codekit-prepend "../../lib/bootstrap/js/transition.js";

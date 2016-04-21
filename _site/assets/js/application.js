95ff49838460fc37d5ae8f95e928657ecad9c604
Chet Farley <chetfarley@gmail.com>
2016-04-21 13:36:31 -0500

    Make data load from google sheets
    Added sheetu integration (https://sheetsu.com/your-apis) and Bootstrap
    Table plugin for sorting and loading message.

--- _site/assets/js/application.js
+++ _site/assets/js/application.js
@@ -1,12 +1,43 @@
 // Individual Bootstrap Scripts
-// @codekit-prepend "../../lib/bootstrap/js/affix.js";
-// @codekit-prepend "../../lib/bootstrap/js/alert.js";
-// @codekit-prepend "../../lib/bootstrap/js/button.js";
-// @codekit-prepend "../../lib/bootstrap/js/carousel.js";
-// @codekit-prepend "../../lib/bootstrap/js/dropdown.js";
-// @codekit-prepend "../../lib/bootstrap/js/modal.js";
-// @codekit-prepend "../../lib/bootstrap/js/popover.js";
-// @codekit-prepend "../../lib/bootstrap/js/scrollspy.js";
-// @codekit-prepend "../../lib/bootstrap/js/tab.js";
-// @codekit-prepend "../../lib/bootstrap/js/tooltip.js";
-// @codekit-prepend "../../lib/bootstrap/js/transition.js";
+// @codekit-prepend "../../lib/bootstrap-table/bootstrap-table.js";
+
+// Available Scripts
+// "../../lib/bootstrap/js/affix.js";
+// "../../lib/bootstrap/js/alert.js";
+// "../../lib/bootstrap/js/button.js";
+// "../../lib/bootstrap/js/carousel.js";
+// "../../lib/bootstrap/js/dropdown.js";
+// "../../lib/bootstrap/js/modal.js";
+// "../../lib/bootstrap/js/tooltip.js";
+// "../../lib/bootstrap/js/popover.js";
+// "../../lib/bootstrap/js/scrollspy.js";
+// "../../lib/bootstrap/js/tab.js";
+// "../../lib/bootstrap/js/transition.js";
+
+// items spreadsheet
+var sheetsuUrl = "https://sheetsu.com/apis/v1.0/f29e79d5?fields=id,name,type,rarity,attunement,source";
+
+$('#items-table').bootstrapTable({
+    url: sheetsuUrl,
+    columns: [{
+        field: 'name',
+        title: 'Name',
+        sortable: true
+    }, {
+        field: 'type',
+        title: 'Type',
+        sortable: true
+    }, {
+        field: 'rarity',
+        title: 'Rarity',
+        sortable: true
+    }, {
+        field: 'attunement',
+        title: 'Requires Attunement',
+        sortable: true
+    }, {
+        field: 'source',
+        title: 'Source',
+        sortable: true
+    }, ]
+});
